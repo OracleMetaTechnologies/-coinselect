c5c3508b66f8277bd904d23f357ec9663d6e4431
Oracle Meta Technologies <130051349+OracleMetaTechnologies@users.noreply.github.com>
2023-04-25 15:19:59 +0300

    Create index.js

---
+++ stats/index.js
@@ -0,0 +1,6 @@
+let Simulation = require('./simulation')
+let modules = require('./strategies')
+let min = 14226 // 0.1 USD
+let max = 142251558 // 1000 USD
+let feeRate = 56 * 100
+let results = []
