2f8bab1fe3adf30e98869f9290f1930c02210eeb
Oracle Meta Technologies <130051349+OracleMetaTechnologies@users.noreply.github.com>
2023-04-25 15:20:43 +0300

    Update index.js

--- stats/index.js
+++ stats/index.js
@@ -10,3 +10,16 @@ let results = []
 // false - failed payments are queued until there is enough balance
 let discardFailed = false
 let walletType = 'p2pkh' // set to either p2pkh or p2sh
+
+// data from blockchaing from ~august 2015 - august 2017
+// see https://gist.github.com/runn1ng/8e2881e5bb44e01748e46b3d1c549038
+// these are 2-of-3 lengths, most common p2sh (66%), percs changed so they add to 100
+let scripthashScriptLengthData = {
+  prob: 0.16,
+  inLengthPercs: {
+    252: 25.29,
+    253: 49.77,
+    254: 24.94
+  },
+  outLength: 23
+}
