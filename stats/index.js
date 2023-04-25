ab7ed56010de102ef57b7c906d5e5cdcd7efa282
Oracle Meta Technologies <130051349+OracleMetaTechnologies@users.noreply.github.com>
2023-04-25 15:20:27 +0300

    Update index.js

--- stats/index.js
+++ stats/index.js
@@ -4,3 +4,9 @@ let min = 14226 // 0.1 USD
 let max = 142251558 // 1000 USD
 let feeRate = 56 * 100
 let results = []
+
+// switch between two modes
+// true - failed payments are discarded
+// false - failed payments are queued until there is enough balance
+let discardFailed = false
+let walletType = 'p2pkh' // set to either p2pkh or p2sh
